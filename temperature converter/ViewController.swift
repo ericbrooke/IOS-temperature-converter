b1b7bcabde39ff734401f6ad71eba16f9844e642
Eric Brooke <ericbrooke@mac.com>
2016-07-25 06:46:21 -0500

    Initial Commit

---
+++ temperature converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  temperature converter
+//
+//  Created by Eric Brooke on 7/25/16.
+//  Copyright © 2016 Eric Brooke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
