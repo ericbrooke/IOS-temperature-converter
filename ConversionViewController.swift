98eaafea405c6dfdfe3f290d26fdfe1fc37ffe44
Eric Brooke <ericbrooke@mac.com>
2016-07-25 15:52:18 -0500

    Conversion is a thing

--- ConversionViewController.swift
+++ ConversionViewController.swift
@@ -13,15 +13,37 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
-    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
-        if let text = textField.text where !text.isEmpty {
-            celsiusLabel.text = text
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
         } else {
-            celsiusLabel.text = "???"
+            return nil
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
         }
     }
     
     @IBAction func dismissKeyboard(sender: AnyObject) {
         textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = "\(value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 }
