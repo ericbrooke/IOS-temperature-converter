f68802db0ecb13431f4394d2eda6c9e421acc32d
Eric Brooke <ericbrooke@mac.com>
2016-07-25 10:30:06 -0500

    WIP - New Controller and keyboard

---
+++ ConversionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ConversionViewController.swift
+//  temperature converter
+//
+//  Created by Eric Brooke on 7/25/16.
+//  Copyright © 2016 Eric Brooke. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text where !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+}
